efede928cadbb6c92272c8b65dde7a25d5658c3a
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2020-05-17 22:57:52 +0700

    Changelog 1.0.8 : - Update default config

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $gmt = 7; // If your server datetime located in south east, so it means GMT+7.
 
 // firewall
 $firewall_origin = false;   // this to activate firewall (default is false)
-$allow_no_referer = false;  // allow access for no-referer policy (default is false)
+$allow_no_referer = true;  // allow access in html img tag (default is true)
 $allow_origin = [
     'https://yourdomain1.com',
     'https://yourdomain2.com'
